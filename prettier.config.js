f941363b6908921f727fbf998d036f61ece38992
Romulo Barbosa <romulo.brb@gmail.com>
2021-01-14 21:01:02 -0300

    Configure eslint

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	singleQuote: true, // Força utilização de aspas simples
+	trailingComma: 'all', // Força utilização da virgula no fim de objetos
+	arrowParens: 'avoid', // Força não utilização de parênteses com funções de um único parâmetro.
+	printWidth: 120,
+	tabWidth: 2,
+	useTabs: false,
+};
